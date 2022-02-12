df88bea262435c66f9e17b625f6dca9a051d733b
Wojciech Siewierski <wojciech@siewierski.eu>
2022-02-13 00:57:24 +0100

    Add a very basic Nix derivation

---
+++ default.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+with pkgs;
+with python3Packages;
+buildPythonApplication rec {
+  pname = "clipboard-actions";
+  version = "0.9";
+  src = lib.cleanSource ./.;
+
+  propagatedBuildInputs = [
+    httpx xsel xclip
+  ];
+}
